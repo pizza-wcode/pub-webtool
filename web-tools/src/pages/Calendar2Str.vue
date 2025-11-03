c6535f1616c38557e08f81956d80b807446ca5dd
pizza-wcode <pizza.gituser@gmail.com>
2025-11-03 16:59:58 +0900

    Calendar2Strを文字列改善

--- web-tools/src/pages/Calendar2Str.vue
+++ web-tools/src/pages/Calendar2Str.vue
@@ -2,6 +2,7 @@
   <q-page class="q-ma-lg">
     <div class="row items-start justify-start q-col-gutter-md">
       <div class="col-12 col-md-auto">
+        <div class="text-bold"> 日付選択</div>
         <q-date v-model="dateval" :landscape="true"></q-date>
         <br />
         <span style="color: grey;">
@@ -12,7 +13,7 @@
       </div>
       <div class="col-12 col-md">
         <div class="row q-col-gutter-md">
-          <div class="col-12 text-bold"> 文字列をクリップボードにコピー</div>
+          <div class="col-12 text-bold">日付文字をクリップボードにコピー</div>
           <div class="col-12"> <q-btn color="primary" icon="content_copy" @click="() => copyClipDate('M/D(ddd)')">{{
             date.formatDate(dateval, 'M/D(ddd)') }}</q-btn></div>
           <div class="col-12"> <q-btn color="primary" icon="content_copy"
@@ -23,22 +24,22 @@
         </div>
       </div>
     </div>
-    <div class="row items-start justify-start q-col-gutter-md">
+    <div class="row items-start justify-start q-col-gutter-md q-mt-lg">
+
 
       <div class="col-12 col-md-auto">
-        <q-checkbox v-model="setTime" class="q-mt-md">時刻もセット</q-checkbox><br />
-        <div v-if="setTime">
-          <q-time v-model="timeval" :landscape="true" :format24h="true"
-            :minute-options="[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]"></q-time>
-          <br />
-          <span style="color: grey;">
-            時、分の順番に数字をクリックして選択<br />
-            分を選択するタイミングで時を変更したい時は<br />左側の青いパネルで色が薄くなっている時の数字をクリック</span>
-        </div>
+        <div class="text-bold"> 時刻選択</div>
+        <q-time v-model="timeval" :landscape="true" :format24h="true"
+          :minute-options="[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]"></q-time>
+        <br />
+        <span style="color: grey;">
+          時、分の順番に数字をクリックして選択<br />
+          分を選択するタイミングで時を変更したい時は<br />左側の青いパネルで色が薄くなっている時の数字をクリック</span>
+
       </div>
-      <div class="col-12 col-md" v-if="setTime">
+      <div class="col-12 col-md">
         <div class="row q-col-gutter-md" v-if="timeval">
-          <div class="col-12 text-bold"> 文字列をクリップボードにコピー</div>
+          <div class="col-12 text-bold">日付文字をクリップボードにコピー</div>
           <div class="col-12"> <q-btn color="primary" icon="content_copy"
               @click="() => copyToClipboard(date.formatDate(dateval + ' ' + timeval, 'M/D(ddd) HH:mm～'))">{{
                 date.formatDate(dateval + ' ' + timeval, 'M/D(ddd) HH:mm～') }}</q-btn></div>
@@ -61,7 +62,6 @@ import enUS from 'quasar/lang/en-US.js'
 
 const dateval = ref(date.formatDate(new Date(), 'YYYY/MM/DD'))
 const timeval = ref(null)
-const setTime = ref(false)
 const $q = useQuasar();
 
 const copyClipDate = async (format: string, locale?: DateLocale) => {
